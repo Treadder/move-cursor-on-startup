76405f0abc7b3f1a82a59f14c7da2ecc3248d72c
Jared Kelnhofer <sawredjet@gmail.com>
2025-05-03 22:38:21 -0700

    initial commit

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { MarkdownView, Plugin, } from 'obsidian';
+
+
+
+
+export default class HelloWorldPlugin extends Plugin {
+
+	async onload() {
+
+		let firstNoteOpened = false;
+
+		this.registerEvent(
+			this.app.workspace.on('file-open', (file) => {
+				if (!firstNoteOpened && file && file.extension === 'md') {
+					const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+					if (activeView) {
+						setTimeout(() => { // slight delay
+							activeView.editor.exec('goRight');
+							activeView.editor.exec('goLeft');
+						}, 50);
+						firstNoteOpened = true;
+					}
+				}
+			})
+		);
+	}
+}
